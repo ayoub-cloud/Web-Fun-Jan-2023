0f2533be3f68220716163e60ac5a56d6d351eb8e
ayoub-cloud <ayoubb06@hotmail.com>
2023-05-09 12:34:32 +0200

    fizzbuzz+loop

---
+++ Week2/Day 1/Practice/LoopChallenges.js
@@ -0,0 +1,18 @@
+// 1-Print odds 1-20
+
+for(var i=1; i<=20; i++) 
+  if(i% 2!=0)    {
+    console.log(i); }
+    // 2-Decreasing Multiples of 3
+
+    for (var i=100 ; i>=0 ; i--)
+    if( i%3 != 100 ) ;
+    {console.log(i);
+
+    }
+    // 3 Print the sequence
+    for ( i=4 ; i>=-3.5 ; i-= 1.5) 
+    console.log(i)
+    // Sigma
+    
+    // Factorial
