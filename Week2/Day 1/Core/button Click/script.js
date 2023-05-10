f412a1761816b6b42d3fbf9f2934e3538269b92e
ayoub-cloud <ayoubb06@hotmail.com>
2023-05-10 12:25:53 +0200

    button Click

---
+++ Week2/Day 1/Core/button Click/script.js
@@ -0,0 +1,10 @@
+function change(element){
+    element.innerText = "Logout";
+}
+function addDefinition(element) {
+    element.remove()
+}
+
+function alertMessage(){
+    alert("Ninja was liked");
+}
